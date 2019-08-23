780d49606720dff6def849b4415a056dcc325d97
endormi <39559256+endormi@users.noreply.github.com>
2019-08-23 12:32:10 +0300

    Clean organizer filetypes

--- organize-files/organizer.py
+++ organize-files/organizer.py
@@ -23,55 +23,21 @@ compressed_files = r'destination/for/compressed'
 os.chdir(PATH)
 move_file = os.listdir(PATH)
 
-try:
-    for mv in move_file:
-        # filetypes
-        if mv.endswith('.png'):
-            shutil.move(mv, images)
-        if mv.endswith('.jpg'):
-            shutil.move(mv, images)
-        if mv.endswith('.jpeg'):
-            shutil.move(mv, images)
-        if mv.endswith('.gif'):
-            shutil.move(mv, images)
-        if mv.endswith('.mp3'):
-            shutil.move(mv, audio)
-        if mv.endswith('.wav'):
-            shutil.move(mv, audio)
-        if mv.endswith('.mid'):
-            shutil.move(mv, audio)
-        if mv.endswith('.pdf'):
-            shutil.move(mv, pdf_doc)
-        if mv.endswith('.doc'):
-            shutil.move(mv, pdf_doc)
-        if mv.endswith('.docx'):
-            shutil.move(mv, pdf_doc)
-        if mv.endswith('.docxml'):
-            shutil.move(mv, pdf_doc)
-        if mv.endswith('.txt'):
-            shutil.move(mv, texts)
-        if mv.endswith('.text'):
-            shutil.move(mv, texts)
-        if mv.endswith('.note'):
-            shutil.move(mv, texts)
-        if mv.endswith('.mp4'):
-            shutil.move(mv, videos)
-        if mv.endswith('.avi'):
-            shutil.move(mv, videos)
-        if mv.endswith('.mov'):
-            shutil.move(mv, videos)
-        if mv.endswith('.wmw'):
-            shutil.move(mv, videos)
-        if mv.endswith('.flv'):
-            shutil.move(mv, videos)
-        if mv.endswith('.zip'):
-            shutil.move(mv, compressed_files)
-        if mv.endswith('.rar'):
-            shutil.move(mv, compressed_files)
-        if mv.endswith('.tar'):
-            shutil.move(mv, compressed_files)
-        if mv.endswith('.7z'):
-            shutil.move(mv, compressed_files)
+
+for mv in move_file:
+    # filetypes
+    if mv.endswith(('.png', '.jpg', '.jpeg', 'gif')):
+        shutil.move(mv, images)
+    if mv.endswith(('.mp3', '.wav', '.mid')):
+        shutil.move(mv, audio)
+    if mv.endswith(('.pdf', '.doc', '.docx', '.docxml')):
+        shutil.move(mv, pdf_doc)
+    if mv.endswith(('.txt', '.text', '.note')):
+        shutil.move(mv, texts)
+    if mv.endswith(('.mp4', '.avi', '.mov', '.vmw', '.flv')):
+        shutil.move(mv, videos)
+    if mv.endswith(('.zip', '.rar', '.tar', '.7z')):
+        shutil.move(mv, compressed_files)
 
     print("Organizing files!")
 
@@ -83,5 +49,4 @@ try:
     time.sleep(2)
     print(".")
     time.sleep(2)
-finally:
     print("Finished moving files!")
