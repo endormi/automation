c7f864811addeac1e598a04fae89529bb8e19301
endormi <39559256+endormi@users.noreply.github.com>
2019-10-24 19:47:49 +0300

    Add whatsapp message

---
+++ whatsapp/send-msg.py
@@ -0,0 +1,30 @@
+"""
+
+author: @endormi
+
+Automated text sender for Whatsapp
+
+"""
+
+from selenium import webdriver
+import time
+
+
+browser = webdriver.Chrome(r'file/to/chromedriver/if/needed/otherwise/remove/r')
+browser.get('https://web.whatsapp.com')
+
+name = ''
+message = ''
+amount_of_times = int('')
+
+time.sleep(1)
+
+user = browser.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+user.click()
+send_msg = browser.find_element_by_class_name('_13mgZ')
+
+
+for x in range(amount_of_times):
+    send_msg.send_keys(message)
+    btn = browser.find_element_by_class_name('_3M-N-')
+    btn.click()
