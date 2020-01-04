54ea167c4b8634d3ab50b972853372ce747ddc31
endormi <39559256+endormi@users.noreply.github.com>
2020-01-04 10:31:52 +0200

    Update organizer.py
    * Add if folder doesn't exist, create a new a dir
    
    * Add print of found files and use organize() function

--- organize-files/organizer.py
+++ organize-files/organizer.py
@@ -24,29 +24,55 @@ os.chdir(PATH)
 move_file = os.listdir(PATH)
 
 
-for mv in move_file:
-    # filetypes
-    if mv.endswith(('.png', '.jpg', '.jpeg', '.gif', '.ico', '.svg')):
-        shutil.move(mv, images)
-    if mv.endswith(('.mp3', '.wav', '.mid', '.midi', '.ogg')):
-        shutil.move(mv, audio)
-    if mv.endswith(('.pdf', '.doc', '.docx', '.docxml')):
-        shutil.move(mv, docs)
-    if mv.endswith(('.txt', '.text', '.note', '.rtf')):
-        shutil.move(mv, texts)
-    if mv.endswith(('.mp4', '.avi', '.mov', '.vmw', '.flv', '.mpeg', '.mpg')):
-        shutil.move(mv, videos)
-    if mv.endswith(('.zip', '.rar', '.tar', '.7z', '.pkg')):
-        shutil.move(mv, compressed_files)
+def organize():
 
-print("Organizing files!")
+    print("Organizing files!")
 
-# Animation
-time.sleep(2)
-print(".")
-time.sleep(2)
-print(".")
-time.sleep(2)
-print(".")
-time.sleep(2)
-print("Finished organizing files!")
+    for mv in move_file:
+        # filetypes
+        if mv.endswith(('png', 'jpg', 'jpeg', 'gif', 'ico', 'svg')):
+            if not os.path.exists(f"{images}"):
+                os.mkdir(f"{images}")
+            shutil.move(mv, images)
+        if mv.endswith(('mp3', 'wav', 'mid', 'midi', 'ogg')):
+            if not os.path.exists(f"{audio}"):
+                os.mkdir(f"{audio}")
+            shutil.move(mv, audio)
+        if mv.endswith(('pdf', 'doc', 'docx', 'docxml')):
+            if not os.path.exists(f"{docs}"):
+                os.mkdir(f"{docs}")
+            shutil.move(mv, docs)
+        if mv.endswith(('txt', 'text', 'note', 'rtf')):
+            if not os.path.exists(f"{texts}"):
+                os.mkdir(f"{texts}")
+            shutil.move(mv, texts)
+        if mv.endswith(('mp4', 'avi', 'mov', 'vmw', 'flv', 'mpeg', 'mpg')):
+            if not os.path.exists(f"{videos}"):
+                os.mkdir(f"{videos}")
+            shutil.move(mv, videos)
+        if mv.endswith(('zip', 'rar', 'tar', '7z', 'pkg')):
+            if not os.path.exists(f"{compressed_files}"):
+                os.mkdir(f"{compressed_files}")
+            shutil.move(mv, compressed_files)
+
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print(".")
+    time.sleep(1)
+    print("Finished organizing files!")
+
+
+def main():
+
+    if len(move_file) > 0:
+        print(f"Found {len(move_file)} file(s). \n")
+        organize()
+
+    else:
+        print("No files found!")
+        print("Check your path: " + PATH)
+
+
+main()
