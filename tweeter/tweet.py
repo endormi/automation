81709da5b398c85fcfd6b90076583baf679a1e82
Erno Salo <39559256+endormi@users.noreply.github.com>
2020-01-31 20:33:11 +0200

    Update tweet.py

--- tweeter/tweet.py
+++ tweeter/tweet.py
@@ -18,17 +18,14 @@ browser = webdriver.Chrome(r'file/to/driver/if/needed')
 browser.get('https://twitter.com/login')
 
 
-user = input("Username: ")
-username = f'{user}'
-password = input("Password: ")
-__password__ = f'{password}'
-tweet = input("Tweet: ")
-__tweet__ = f'{tweet}'
+username = ''
+password = ''
+tweet = ''
 
 user = browser.find_element_by_class_name('js-username-field')
 user.send_keys(username)
 pw = browser.find_element_by_class_name('js-password-field')
-pw.send_keys(__password__)
+pw.send_keys(password)
 log_in = browser.find_element_by_class_name('EdgeButtom--medium')
 log_in.click()
 time.sleep(1)
@@ -36,7 +33,7 @@ start_tweeting = browser.find_element_by_class_name('r-e7q0ms')
 start_tweeting.click()
 time.sleep(1)
 tweet_content = browser.find_element_by_class_name('notranslate')
-tweet_content.send_keys(__tweet__)
+tweet_content.send_keys(tweet)
 time.sleep(1)
 tweet_button = browser.find_element_by_class_name('r-1n0xq6e')
 tweet_button.click()
