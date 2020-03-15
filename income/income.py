9623faf0ef3376ae02100d96c5005bb3135a68fa
endormi <ernosalo14@gmail.com>
2020-03-15 18:30:21 +0200

    Update income.py

--- income/income.py
+++ income/income.py
@@ -13,6 +13,9 @@ wb = xlsxwriter.Workbook('filename.xlsx', {'strings_to_numbers': True})
 wsb = wb.add_worksheet(name='Income')
 bold = wb.add_format({'bold': True})
 
+row = 2
+col = 0
+
 wsb.write('A1', 'Income', bold)
 wsb.write('A2', 'Date:', bold)
 wsb.write('B2', 'Type of income:', bold)
@@ -21,21 +24,21 @@ wsb.write('E2', 'Total:', bold)
 
 currency_format = wb.add_format({'num_format': '€#,##0.00'})
 
-wsb.write('A3', '01.01.2020')
-wsb.write('B3', 'type1')
-wsb.write('C3', '1000')
-wsb.write('A4', '01.01.2020')
-wsb.write('B4', 'type2')
-wsb.write('C4', '1000')
-wsb.write('A5', '01.01.2020')
-wsb.write('B5', 'type2')
-wsb.write('C5', '1000')
-wsb.write('A6', '01.01.2020')
-wsb.write('B6', 'type2')
-wsb.write('C6', '1000')
-wsb.write('A7', '01.01.2020')
-wsb.write('B7', 'type2')
-wsb.write('C7', '1000')
+income = (
+    ['01.01.2020', 'type1', '1000'],
+    ['01.01.2020', 'type2', '1000'],
+    ['01.01.2020', 'type3', '1000'],
+    ['01.01.2020', 'type4', '1000'],
+    ['01.01.2020', 'type4', '1000'],
+    ['01.01.2020', 'type5', '1000'],
+)
+
+for i, c, a in (income):
+    wsb.write(row, col, i)
+    wsb.write(row, col + 1, c)
+    wsb.write(row, col + 2, a)
+    row += 1
+
 wsb.write_formula("E3", "=SUM(C3:C7)", currency_format)
 
 wb.close()
