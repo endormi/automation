f9342d8425af7842f08f033b5a79be0b7f7a923a
endormi <39559256+endormi@users.noreply.github.com>
2022-10-22 19:54:58 +0300

    Use tweepy and env variables

--- tweeter/tweet.py
+++ tweeter/tweet.py
@@ -1,38 +1,26 @@
 """
 
 author: @endormi
+
 Automated tweeter
 
 """
 
-from selenium import webdriver
-import time
+import tweepy
+import os
 
 
-"""
-Install browser driver:
-https://www.seleniumhq.org/download/
-"""
+auth = tweepy.OAuthHandler(os.getenv("TWITTER_API_KEY"), os.getenv("TWITTER_API_SECRET_KEY"))
+auth.set_access_token(os.getenv("TWITTER_ACCESS_TOKEN"), os.getenv("TWITTER_ACCESS_TOKEN_SECRET"))
 
-username = input("Userame: ")
-password = input("Password: ")
-tweet = input("Tweet: ")
+api = tweepy.API(auth)
+tweet = str(input("Tweet: \n"))
 
-browser = webdriver.Chrome(r'file/to/driver/if/needed')
-browser.get('https://twitter.com/login')
-
-user = browser.find_element_by_class_name('js-username-field')
-user.send_keys(username)
-pw = browser.find_element_by_class_name('js-password-field')
-pw.send_keys(password)
-log_in = browser.find_element_by_class_name('EdgeButtom--medium')
-log_in.click()
-time.sleep(1)
-start_tweeting = browser.find_element_by_class_name('r-e7q0ms')
-start_tweeting.click()
-time.sleep(1)
-tweet_content = browser.find_element_by_class_name('notranslate')
-tweet_content.send_keys(tweet)
-time.sleep(1)
-tweet_button = browser.find_element_by_class_name('r-1n0xq6e')
-tweet_button.click()
+try:
+    api.update_status(tweet)
+    print("Tweeted!")
+except tweepy.TweepError as error:
+    if error.api_code == 187:
+        print('Tweet already exists.')
+    else:
+        raise error
