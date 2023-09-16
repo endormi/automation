dee4c1847a6f98abdbe25e4576b8e9b48b9ae3d2
adhirajkoo <adhiraj.pandey@kooapp.com>
2023-09-17 00:24:46 +0530

    Add Reddit Scripts

---
+++ reddit/join.py
@@ -0,0 +1,35 @@
+import praw
+
+#Secondary Account Details
+client_id = ""
+client_secret = ""
+user_agent = ""
+username = ""
+password = ""
+
+reddit = praw.Reddit(
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent=user_agent,
+    username=username,
+    password=password
+)
+
+# Read from subs.txt
+subslist = []
+with open("subs.txt", "r") as f:
+    for line in f:
+        subslist.append(line.strip())
+
+# Join one by one and write a log to joined.txt
+with open("joinedlogs.txt", "w") as f:
+    for sub in subslist:
+        try:
+            reddit.subreddit(sub).subscribe()
+            f.write("Joined " + sub + "\n")
+            print("Joined " + sub)
+        except Exception as e:
+            f.write("Failed to join " + sub + "\n")
+            print("Failed to join " + sub)
+
+print("Done")
